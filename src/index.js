ab1b69ee92240598c9b432488ac2471e313452c1
lucas <lucasmengue96@hotmail.com>
2021-10-28 14:45:54 -0300

    criando um array com dados fictícios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import express from "express";
 // guardando o express na constante app
 const app = express();
 
+// criando um array de customers
+const customers = [
+  { id: 1, name: "Marcos Vinicius" },
+  { id: 2, name: "Maria Eduarda" },
+  { id: 3, name: "José Francisco" },
+];
+
+// transformando em JSON
+app.use(express.json());
+
 // criando primeira rota GET respondendo com uma mensagem
 app.get("/api/ping", (request, response) => {
   response.send({
